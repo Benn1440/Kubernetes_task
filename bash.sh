79bce0b8c145f1146fb16c66d48a2393c7013b22
root <root@DESKTOP-9453D0U.localdomain>
2023-09-17 12:49:47 +0100

    updated the kind deployment bash script

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Defuining the name of the Kind cluster
-CLUSTER_NAME="Ben_kind cluster"
+CLUSTER_NAME="kubecluster"
 
 # Check if KinD is installed
 if ! command -v kind &>/dev/null; then
@@ -10,14 +10,17 @@ if ! command -v kind &>/dev/null; then
 fi
 
 # Check if the cluster already exists
-if kind get clusters | grep -q "$CLUSTER_NAME"; then
-  echo "Cluster $CLUSTER_NAME already exists. Deleting it..."
-  kind delete cluster --name "$CLUSTER_NAME"
-fi
+# if kind get clusters | grep -q "$CLUSTER_NAME"; then
+#   echo "Cluster $CLUSTER_NAME already exists. Deleting it..."
+#   kind delete cluster --name "$CLUSTER_NAME"
+# fi
 
 # Create a new KinD cluster
 echo "Creating a new KinD cluster: $CLUSTER_NAME"
 kind create cluster --name "$CLUSTER_NAME"
 
 # Configure kubectl to use the new cluster
+kubectl config use-context $CLUSTER_NAME
 kubectl cluster-info --context "kind-$CLUSTER_NAME"
+
+echo "KinD cluster '$CLUSTER_NAME' has been successfully deployed
